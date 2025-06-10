a7f8ba765add05ffd110855418090a21ac297c48
Arnav Raj <arnavrajcodes@gmail.com>
2025-06-10 23:19:52 +0530

    errors

--- frontend/src/pages/home/Home-Page-Components/Featured.tsx
+++ frontend/src/pages/home/Home-Page-Components/Featured.tsx
@@ -18,7 +18,7 @@ const Featured = () => {
   return (
     <div className="space-y-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {featuredSong
+        {Array.isArray(featuredSong) 
           ? featuredSong.map((song, index) => {
               return (
                 <div
