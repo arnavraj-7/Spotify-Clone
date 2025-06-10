ff71d9face5d6ef175fc6799616087127ad27070
Arnav Raj <arnavrajcodes@gmail.com>
2025-06-10 22:42:09 +0530

    overriding errors of ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+
   server:{port:3000},
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), checker({ typescript: false })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
